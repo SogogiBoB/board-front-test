0251641a5060e2216416c4bd9b000ad0146f8338
SogogiBoB <jjh9240@naver.com>
2021-12-13 23:28:53 +0900

    board-react first commit

---
+++ board-app/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'; 
+import ListBoardComponent from './components/ListBoardComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
+
+function App() {
+  return (
+    <div> 
+      <Router>
+        <HeaderComponent/>
+          <div className="container">
+            <Routes>
+              <Route path = "/" exact element = {<ListBoardComponent/>}></Route>
+              <Route path = "/board" element = {<ListBoardComponent/>}></Route>
+            </Routes>
+          </div>
+        <FooterComponent/>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ board-app/src/components/ListBoardComponent.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import BoardService from '../service/BoardService';
+
+class ListBoardComponent extends Component {
+    constructor(props) {
+        super(props)
+    // # 1. 
+        this.state = { 
+            boards: []
+        }
+		
+    }
+    // # 2. 
+    componentDidMount() {
+        BoardService.getBoards().then((res) => {
+            this.setState({ boards: res.data});
+        });
+    }
+
+    // # 3.
+    render() {
+        return (
+            <div>
+                <h2 className="text-center">Boards List</h2>
+                <div className ="row">
+                    <table className="table table-striped table-bordered">
+                        <thead>
+                            <tr>
+                                <th>글 번호</th>
+                                <th>제목 </th>
+                                <th>내용 </th>
+                                <th>작성자 </th>
+                                <th>조회수</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                this.state.boards.map(
+                                    board => 
+                                    <tr key = {board}>
+                                        <td> {board.uid} </td>
+                                        <td> {board.title} </td>
+                                        <td> {board.content} </td>
+                                        <td> {board.writer} </td>
+                                        <td> {board.viewCnt} </td>
+                                    </tr>
+                                )
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ListBoardComponent;
